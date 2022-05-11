6080267b6fa2e42970d327561edf268cc530d469
EGoesche <e.goesche@outlook.de>
2022-05-11 14:36:17 +0200

    Finish waves

--- src/main/java/mt/CosineWave.java
+++ src/main/java/mt/CosineWave.java
@@ -2,11 +2,12 @@ package mt;
 
 public class CosineWave extends Signal {
 
-    public CosineWave(int length, String name, int k) {
-        super(length, name);
+    public CosineWave(int length, int k) {
+        super(length, "Cosine");
         float buffer[] = new float[length];
         for (int i = 0; i < length; i++) {
             buffer[i] = (float) Math.cos((k / length) * 2 * Math.PI);
         }
+        this.buffer = buffer;
     }
 }
--- src/main/java/mt/SineWave.java
+++ src/main/java/mt/SineWave.java
@@ -4,13 +4,12 @@ import java.lang.Math;
 
 public class SineWave extends Signal {
 
-    public SineWave(int length, String name, int k) {
-        super(length, name);
+    public SineWave(int length, int k) {
+        super(length, "Sine");
         float buffer[] = new float[length];
         for (int i = 0; i < length; i++) {
             buffer[i] = (float) Math.sin((k / length) * 2 * Math.PI);
         }
-
-
+        this.buffer = buffer;
     }
 }
