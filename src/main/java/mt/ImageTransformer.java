2abede0d8dab10feb965e5462c320ff983c8648a
EGoesche <e.goesche@outlook.de>
2022-06-14 17:56:25 +0200

    Update ImageTransformer.java

--- src/main/java/mt/ImageTransformer.java
+++ src/main/java/mt/ImageTransformer.java
@@ -20,6 +20,23 @@ public class ImageTransformer implements ImageFilter {
     }
 
     public void apply(Image input, Image output) {
-        
+        for (int yPrime = output.minIndexY; yPrime < output.minIndexY + output.height(); yPrime++) {
+            for (int xPrime = output.minIndexX; xPrime < output.minIndexX + output.width(); xPrime++) {
+                // apply translation
+                float x = xPrime - shiftX;
+                float y = yPrime - shiftY;
+
+                // apply rotation
+                x = (float) (x * Math.cos(rotation) + y * Math.sin(rotation));
+                y = (float) - (x * Math.sin(rotation) + y * Math.cos(rotation));
+
+                // apply scaling
+                x /= scale;
+                y /= scale;
+
+                // set output image value
+                output.setAtIndex(xPrime, yPrime, input.interpolatedAt(x, y));
+            }
+        }
     }
 }
