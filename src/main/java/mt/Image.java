573a3cc61ec8a7e5cb8252703332405f93727ddf
EGoesche <e.goesche@outlook.de>
2022-06-22 14:22:00 +0200

    Update project with solution for ex5

--- src/main/java/mt/Image.java
+++ src/main/java/mt/Image.java
@@ -16,8 +16,7 @@ public class Image extends Signal {
     protected int height;
     protected int minIndexX = 0;
     protected int minIndexY = 0;
-
-    protected float[] origin = new float[]{ 0, 0 };
+    protected float[] origin = new float[] { 0, 0 };
 
     public Image(int width, int height, String name) {
         super(height * width, name);
@@ -32,14 +31,19 @@ public class Image extends Signal {
     }
 
     public void show() {
-        DisplayUtils.showImage(buffer, name, width(), origin, spacing(), /*Replace window with same name*/true);
+        DisplayUtils.showImage(buffer, name, width(), origin, spacing(), true);
     }
+
     public int width() {
         return width;
     }
 
     public int height() {
         return height;
+    }
+
+    public long[] shape() {
+        return new long[] { width, height };
     }
 
     public float atIndex(int x, int y) {
@@ -65,6 +69,24 @@ public class Image extends Signal {
     }
 
 
+    public Image add(Image image){
+        Image result = new Image(width,height, name() + " + " + image.name());
+        result.minIndexX = minIndexX;
+        result.minIndexY = minIndexY;
+
+        if(image.width != width || image.height != height){
+            throw new RuntimeException("Images need to have the same size!");
+        }
+        for (int y = minIndexY; y < minIndexY + height(); ++y) {
+            for (int x = minIndexX; x < minIndexX + width(); ++x) {
+                float plus = image.atIndex(x,y) + atIndex(x,y);
+                result.setAtIndex(x,y,plus);
+
+            }
+        }
+        return result;
+    }
+
     public void setBuffer(float[] buffer) {
         this.buffer = buffer;
     }
@@ -86,70 +108,46 @@ public class Image extends Signal {
         return minIndexY + height() - 1;
     }
 
-    public void fft(){
-        DisplayUtils.FFT(buffer, name, width(), origin, /*spacing()*/ 1.0f);
-    }
-
-
-    public Image add(Image image){
-        Image result = new Image(width,height, name() + " + " + image.name());
-        result.minIndexX = minIndexX;
-        result.minIndexY = minIndexY;
-
-        if(image.width != width || image.height != height){
-            throw new RuntimeException("Images need to have the same size!");
-        }
-        for (int y = minIndexY; y < minIndexY + height(); ++y) {
-            for (int x = minIndexX; x < minIndexX + width(); ++x) {
-                float plus = image.atIndex(x,y) + atIndex(x,y);
-                result.setAtIndex(x,y,plus);
-
-            }
-        }
-        return result;
-    }
-
-    // store the origin points x,y as a class variable
     public void setOrigin(float x, float y) {
         origin[0] = x;
         origin[1] = y;
     }
 
-    // the origin() returns the {x,y} as float array from the stored origin class variable.
     public float[] origin() {
         return origin;
     }
 
-    // Sets the origin to the center of the image
     public void centerOrigin() {
-        origin[0] = width / 2.f;
-        origin[1] = height / 2.f;
+        origin[0] = -width() * spacing * 0.5f;
+        origin[1] = -height() * spacing * 0.5f;
+    }
+
+    private static float lerp(float a, float b, float f) {
+        return a + f * (b - a);
     }
 
     public float interpolatedAt(float x, float y) {
+
         x -= origin[0];
         y -= origin[1];
         x /= spacing;
         y /= spacing;
-        int x1 = (int) Math.floor(x);
-        int x2 = (int) Math.ceil(x);
-        int y1 = (int) Math.floor(x);
-        int y2 = (int) Math.ceil(x);
-        float diff_x_x1 = x - x1;
-        float diff_y_y1 = y - y1;
-        float xu = linearInterpolation(atIndex(x1, y2), atIndex(x2, y2), diff_x_x1);
-        float xl = linearInterpolation(atIndex(x1, y1), atIndex(x2, y1), diff_x_x1);
-        return linearInterpolation(xu, xl, diff_y_y1);
+
+
+        int xFloor = (int) Math.floor(x);
+        int yFloor = (int) Math.floor(y);
+        int xCeil = (int) Math.ceil(x);
+        int yCeil = (int) Math.ceil(y);
+
+        float x1 = lerp(atIndex(xFloor, yFloor), atIndex(xCeil, yFloor), x - xFloor);
+        float x2 = lerp(atIndex(xFloor, yCeil), atIndex(xCeil, yCeil), x - xFloor);
+
+
+        return lerp(x1, x2, y - yFloor);
     }
 
-    // Definition of arguments
-    // diff_x_x1 = x - x_1 compute the difference between point x and x_1
-    // fx_1 = f(x_1), pixel value at point x_1
-    // fx_2 = f(x_2), pixel value at point x_2
-    private float linearInterpolation(float fx_1, float fx_2, float diff_x_x1) {
-        return fx_1 + diff_x_x1 * (fx_2 - fx_1);
+    public void fft(){
+        DisplayUtils.FFT(buffer, name, width(), origin, /*spacing()*/ 1.0f);
     }
-
-
 
 }
--- src/main/java/mt/ImageTransformer.java
+++ src/main/java/mt/ImageTransformer.java
@@ -8,35 +8,54 @@ package mt;
 
 public class ImageTransformer implements ImageFilter {
 
-    // Transformation parameters
-    public float shiftX; // tx
-    public float shiftY; // ty
-    public float rotation; // theta
-    public float scale; // s
-    
+    public float shiftX;
+    public float shiftY;
+    public float rotation;
+    public float scale;
+
+    public void apply(Image input, Image output) {
+        // This method should do an image transformation in the following order
+        // - translation
+        // - rotation
+        // - scaling
+        // For each index of the output image
+        for (int yPrime = 0; yPrime < output.height(); ++yPrime) {
+            for (int xPrime = 0; xPrime < output.width(); ++xPrime) {
+
+                // Transform output index to physical coordinates
+                float inputX = (float) (xPrime * output.spacing() + output.origin()[0]);
+                float inputY = (float) (yPrime * output.spacing() + output.origin()[1]);
+
+                // Remember that for the inverse translation we have to perform the individual transform in inverse order
+
+
+                // Apply inverse scaling to physical output coordinates
+                inputX *= 1.f / scale;
+                inputY *= 1.f / scale;
+
+
+                // Apply inverse rotation to your intermediate result
+                float tmpX = (float) (Math.cos(rotation) * inputX + Math.sin(rotation) * inputY);
+                float tmpY = (float) (-Math.sin(rotation) * inputX + Math.cos(rotation) * inputY);
+                inputX = tmpX;
+                inputY = tmpY;
+
+
+                // Apply inverse the inverse shift to your intermediate result
+                inputX -= shiftX;
+                inputY -= shiftY;
+
+
+                // Use input.interpolatedAt to get the pixel value at the calculated physical position
+                float value = input.interpolatedAt(inputX, inputY);
+                // Set your result at the current output pixel
+                output.setAtIndex(xPrime, yPrime, value);
+            }
+        }
+    }
+
     @Override
     public String name() {
         return "Image Transformer";
-    }
-
-    public void apply(Image input, Image output) {
-        for (int yPrime = output.minIndexY; yPrime < output.minIndexY + output.height(); yPrime++) {
-            for (int xPrime = output.minIndexX; xPrime < output.minIndexX + output.width(); xPrime++) {
-                // apply translation
-                float x = xPrime - shiftX;
-                float y = yPrime - shiftY;
-
-                // apply rotation
-                x = (float) (x * Math.cos(rotation) + y * Math.sin(rotation));
-                y = (float) - (x * Math.sin(rotation) + y * Math.cos(rotation));
-
-                // apply scaling
-                x /= scale;
-                y /= scale;
-
-                // set output image value
-                output.setAtIndex(xPrime, yPrime, input.interpolatedAt(x, y));
-            }
-        }
     }
 }
